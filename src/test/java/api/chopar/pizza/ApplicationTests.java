6be6b0079e4dd335a160e44333d6573bfe495251
MuzaffarOrtiqov <muzaffarmike2@gmail.com>
2024-07-22 16:50:08 +0500

    Project started

---
+++ src/test/java/api/chopar/pizza/ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.chopar.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
